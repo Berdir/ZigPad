cf1dee8a6c029d5d801c37cc5861c8a96de5f5a1
Sascha Grossenbacher <saschagros@gmail.com>
2011-04-06 13:26:14 +0200

    Added tests target and gh-unit configuration.

---
+++ Tests/ExampleTest.h
@@ -0,0 +1,16 @@
+//
+//  ExampleTest.h
+//  ZigPad
+//
+//  Created by ceesar on 06/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ExampleTest : NSObject {
+    
+}
+
+@end
