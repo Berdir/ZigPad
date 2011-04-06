53b52cea9e2c671d12fcc0aba92e7f8495d6ae9e
Sascha Grossenbacher <saschagros@gmail.com>
2011-04-06 11:18:32 +0200

    Initial Commit

---
+++ ZigPad/RootViewController.h
@@ -0,0 +1,20 @@
+//
+//  RootViewController.h
+//  ZigPad
+//
+//  Created by ceesar on 06/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface RootViewController : UITableViewController <NSFetchedResultsControllerDelegate> {
+
+}
+
+@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+
+@end
---
+++ ZigPad/ZigPadAppDelegate.h
@@ -0,0 +1,26 @@
+//
+//  ZigPadAppDelegate.h
+//  ZigPad
+//
+//  Created by ceesar on 06/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZigPadAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
