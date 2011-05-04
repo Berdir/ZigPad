0849e3f98c005c21ab3080b6b3e9d47f3991d1e9
tazimmer <markus-zimmermann@stud.hslu.ch>
2011-05-04 16:40:50 +0200

    imageview zwischenstatus

---
+++ ZigPad/ImageViewController.h
@@ -0,0 +1,19 @@
+//
+//  ImageViewController.h
+//  ZigPad
+//
+//  Created by ceesar on 5/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ActionViewController.h"
+
+
+@interface ImageViewController : ActionViewController {
+    
+}
+
+@property (nonatomic, retain) IBOutlet UIImageView *myImageView;
+
+@end
