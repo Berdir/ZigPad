5066bc4c9c492e27bfd0975faa48c8bd97961fe9
Sascha Grossenbacher <saschagros@gmail.com>
2011-05-23 11:55:14 +0200

    Documented ZigPadSettings according to doxygen standards.

--- ZigPad/ZigPadSettings.h
+++ ZigPad/ZigPadSettings.h
@@ -8,26 +8,64 @@
 
 #import <Foundation/Foundation.h>
 
-
+/**
+ * Allows to access and store application settings.
+ *
+ * Uses NSUserDefaults to store these permanently.
+ */
 @interface ZigPadSettings : NSObject {
+    /**
+     * NSUserDefaults instance.
+     */
     @private NSUserDefaults *standardUserDefaults;
     
 }
 
+/**
+ * Returns the shared instance of this class.
+ */
 +(ZigPadSettings*) sharedInstance;
 
+/**
+ * Setting for the SimulationMode, YES if on.
+ */
 @property (readwrite,assign) BOOL simulationMode;
+
+/**
+ * Setting for the configurator URL.
+ */
 @property (readwrite,assign) NSString *configuratorURL;
 
+/**
+ * Returns the UIColor instance for the navigationBar.
+ */
 @property (readonly,assign) UIColor *modeColor;
 
+/**
+ * Setting for the HomeServer IP, depends on the simulation mode.
+ */
 @property (readonly,assign) NSString *ip;
+
+/**
+ * Setting for the HomeServer Port, depends on the simulation mode.
+ */
 @property (readonly,assign) int port;
 
-- (NSString *) modeKey: (BOOL) simulationMode;
-
+/**
+ * Allows to set the IP for a specific simulation mode.
+ *
+ * @param ip The IP to be used for this simulation mode.
+ * @param simulationMode YES if this IP is for the simulation mode.
+ */
 -(void)setIP: (NSString *) ip simulationMode: (BOOL) simulationMode;
--(void)setPort: (int) ip simulationMode: (BOOL) simulationMode;
+
+/**
+ * Allows to set the Port for a specific simulation mode.
+ *
+ * @param ip The Port to be used for this simulation mode.
+ * @param simulationMode YES if this Port is for the simulation mode.
+ */
+-(void)setPort: (int) port simulationMode: (BOOL) simulationMode;
 
 
 @end
--- ZigPad/ZigPadSettings.m
+++ ZigPad/ZigPadSettings.m
@@ -7,18 +7,32 @@
 //
 
 #import "ZigPadSettings.h"
-#define SIMULATOR @"Simulator"
-#define CONFIGURATOR @"KonfiguratorURL"
 
+/**
+ * The key for the simulation mode.
+ */
+NSString * const SIMULATOR = @"Simulator";
+
+/**
+ * The key for the configuration URL.
+ */
+NSString * const CONFIGURATOR = @"KonfiguratorURL";
+
+/**
+ * Default configurator URL.
+ */
+NSString * const DEFAULT_CONFIGURATOR_URL = @"http://z.worldempire.ch/1/zigpad/config.xml";
 
 @implementation ZigPadSettings
 
+// Contains the shared instance of the class.
 static ZigPadSettings * _sharedInstance = nil;
 
-+(ZigPadSettings*) sharedInstance {
-    
++(ZigPadSettings*) sharedInstance
+{    
     @synchronized([ZigPadSettings class])
 	{
+        // Check if an instance already exists, if not, create one.
 		if (!_sharedInstance)
 			[[self alloc] init];
         
@@ -32,6 +46,7 @@ static ZigPadSettings * _sharedInstance = nil;
 {
 	@synchronized([ZigPadSettings class])
 	{
+        // Make sure that only a single instance can be created.
 		NSAssert(_sharedInstance == nil, @"Attempted to allocate a second instance of a singleton.");
 		_sharedInstance = [super alloc];
 		return _sharedInstance;
@@ -40,74 +55,109 @@ static ZigPadSettings * _sharedInstance = nil;
 	return nil;
 }
 
--(id)init {
+-(id)init
+{
 	self = [super init];
 	if (self != nil) {
+        // Get the standard userdefaults instance.
 		standardUserDefaults = [NSUserDefaults standardUserDefaults];
 	}
     
 	return self;
 }
 
+/**
+ * Returns the proper key part for IP/Port depending on the simulation mode.
+ * @param simulationMode YES if the key for the simulation mode should be used.
+ */
+- (NSString *) modeKey: (BOOL) simulationMode {
+    return simulationMode ? @"Simulation" : @"Real";
+}
+
+/**
+ * Setter method for the simulationMode property.
+ */
 -(void)setSimulationMode:(BOOL)simulationMode
 {
-    [standardUserDefaults setBool:simulationMode forKey:SIMULATOR];}
+    [standardUserDefaults setBool:simulationMode forKey:SIMULATOR];
+}
 
+/**
+ * Getter method for the simulationMode property.
+ */
 - (BOOL)simulationMode
 {	
     return [standardUserDefaults boolForKey:SIMULATOR];
 }
 
+/**
+ * Setter method for the configurationURL property.
+ */
 -(void)setConfiguratorURL:(NSString *)configuratorURL
 {
-    [standardUserDefaults setObject:configuratorURL forKey:CONFIGURATOR];}
+    [standardUserDefaults setObject:configuratorURL forKey:CONFIGURATOR];
+}
 
+/**
+ * Get method for the configurationURL property.
+ */
 - (NSString *) configuratorURL
 {	
     NSString *url = [standardUserDefaults stringForKey:CONFIGURATOR];
     if (url == nil || [url isEqualToString:@""]) {
-        url = @"http://z.worldempire.ch/1/zigpad/config.xml";
+        // If the current string is empty, use the default.
+        url = DEFAULT_CONFIGURATOR_URL;
     }
     return url;
 }
 
-- (NSString *) modeKey: (BOOL) simulationMode {
-    return simulationMode ? @"Simulation" : @"Real";
-}
-
+/**
+ * Getter method for the IP property.
+ */
 - (NSString *) ip {
     NSString *key = [NSString stringWithFormat:@"%@ServerIp", [self modeKey: self.simulationMode]];
     
     return [standardUserDefaults stringForKey:key];
 }
 
+/**
+ * Getter method for the port property.
+ */
 - (int) port {
     NSString *key = [NSString stringWithFormat:@"%@ServerPort", [self modeKey: self.simulationMode]];
     
     return [standardUserDefaults integerForKey:key];
 }
 
+/**
+ * Setter method for the ip property.
+ */
 -(void)setIP: (NSString *) ip simulationMode: (BOOL) simulationMode {
     NSString *key = [NSString stringWithFormat:@"%@ServerIp", [self modeKey: simulationMode]];
-    
-    NSLog(@"Setting IP for %@ to %@ (%@)", [self modeKey: simulationMode], ip, key);
-    
+  
     [standardUserDefaults setObject:ip forKey:key];
 }
+
+/**
+ * Setter method for the port property.
+ */
 -(void)setPort: (int) port simulationMode: (BOOL) simulationMode {
     NSString *key = [NSString stringWithFormat:@"%@ServerPort", [self modeKey: simulationMode]];
-    
-    NSLog(@"Setting Port for %@ to %d (%@)", [self modeKey: simulationMode], port, key);
-    
+
     [standardUserDefaults setInteger:port forKey:key];
 }
 
+/**
+ * Getter method for the modeColor property.
+ */
 - (UIColor *) modeColor {
     //set navigation bar to red if simulation is on (to warn user)
     if (self.simulationMode) {
+        // If the simulation mode is on, display a red bar.
         return [UIColor redColor];
     }
     else {
+        // If not, return the default color.
         return [UIColor colorWithHue:0.6 saturation:0.33 brightness:0.69 alpha:0];
     }
 }
