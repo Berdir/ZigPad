58afd0b2ce9d397912330052e71b72be09bcd2af
Sascha Grossenbacher <saschagros@gmail.com>
2011-06-09 10:23:01 +0200

    Updated documentation

---
+++ doc/html/navtree.js
@@ -0,0 +1,445 @@
+var NAVTREE =
+[
+  [ "ZigPad", "index.html", [
+    [ "Related Pages", "pages.html", [
+      [ "Todo List", "todo.html", null ]
+    ] ],
+    [ "Data Structures", "annotated.html", [
+      [ "Action", "interface_action.html", null ],
+      [ "ActionViewController", "interface_action_view_controller.html", null ],
+      [ "AnimatorHelper", "interface_animator_helper.html", null ],
+      [ "AsyncReadPacket", "interface_async_read_packet.html", null ],
+      [ "AsyncReceivePacket", "interface_async_receive_packet.html", null ],
+      [ "AsyncSendPacket", "interface_async_send_packet.html", null ],
+      [ "AsyncSpecialPacket", "interface_async_special_packet.html", null ],
+      [ "AsyncTCPSocket", "interface_async_t_c_p_socket.html", null ],
+      [ "AsyncUdpSocket", "interface_async_udp_socket.html", null ],
+      [ "<AsyncUdpSocketDelegate>", "protocol_async_udp_socket_delegate-p.html", null ],
+      [ "AsyncWritePacket", "interface_async_write_packet.html", null ],
+      [ "BWOrderedManagedObject", "interface_b_w_ordered_managed_object.html", null ],
+      [ "BWOrderedValueProxy", "interface_b_w_ordered_value_proxy.html", null ],
+      [ "Commander", "interface_commander.html", null ],
+      [ "CommandViewController", "interface_command_view_controller.html", null ],
+      [ "Config", "interface_config.html", null ],
+      [ "Database", "interface_database.html", null ],
+      [ "DataCache", "interface_data_cache.html", null ],
+      [ "FavoritesViewController", "interface_favorites_view_controller.html", null ],
+      [ "FlowCoverRecord", "interface_flow_cover_record.html", null ],
+      [ "FlowCoverView", "interface_flow_cover_view.html", null ],
+      [ "<FlowCoverViewDelegate>", "protocol_flow_cover_view_delegate-p.html", null ],
+      [ "HudDemoAppDelegate", "interface_hud_demo_app_delegate.html", null ],
+      [ "HudDemoViewController", "interface_hud_demo_view_controller.html", null ],
+      [ "ImageDownloader", "interface_image_downloader.html", null ],
+      [ "ImageViewController", "interface_image_view_controller.html", null ],
+      [ "Importer", "interface_importer.html", null ],
+      [ "LocalPicture", "interface_local_picture.html", null ],
+      [ "MBProgressHUD", "interface_m_b_progress_h_u_d.html", null ],
+      [ "<MBProgressHUDDelegate>", "protocol_m_b_progress_h_u_d_delegate-p.html", null ],
+      [ "MBRoundProgressView", "interface_m_b_round_progress_view.html", null ],
+      [ "NSObject(AsyncSocketDelegate)", "interface_n_s_object_07_async_socket_delegate_08.html", null ],
+      [ "Param", "interface_param.html", null ],
+      [ "Presentation", "interface_presentation.html", null ],
+      [ "RootViewController", "interface_root_view_controller.html", null ],
+      [ "Sequence", "interface_sequence.html", null ],
+      [ "SequenceChoiceViewController", "interface_sequence_choice_view_controller.html", null ],
+      [ "SettingsViewController", "interface_settings_view_controller.html", null ],
+      [ "statement", "classstatement.html", null ],
+      [ "SyncEvent", "interface_sync_event.html", null ],
+      [ "Synchronizer", "interface_synchronizer.html", null ],
+      [ "SynchronizerConnection", "interface_synchronizer_connection.html", null ],
+      [ "TCPCommander", "interface_t_c_p_commander.html", null ],
+      [ "TCPServer", "interface_t_c_p_server.html", null ],
+      [ "<TCPServerDelegate>", "protocol_t_c_p_server_delegate-p.html", null ],
+      [ "UDPCommander", "interface_u_d_p_commander.html", null ],
+      [ "VideoViewController", "interface_video_view_controller.html", null ],
+      [ "WebcamViewController", "interface_webcam_view_controller.html", null ],
+      [ "ZigPadAppDelegate", "interface_zig_pad_app_delegate.html", null ],
+      [ "ZigPadSettings", "interface_zig_pad_settings.html", null ]
+    ] ],
+    [ "Data Structure Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "ActionViewController", "interface_action_view_controller.html", [
+        [ "CommandViewController", "interface_command_view_controller.html", null ],
+        [ "ImageViewController", "interface_image_view_controller.html", null ],
+        [ "VideoViewController", "interface_video_view_controller.html", null ],
+        [ "WebcamViewController", "interface_webcam_view_controller.html", null ]
+      ] ],
+      [ "AnimatorHelper", "interface_animator_helper.html", null ],
+      [ "AsyncReadPacket", "interface_async_read_packet.html", null ],
+      [ "AsyncReceivePacket", "interface_async_receive_packet.html", null ],
+      [ "AsyncSendPacket", "interface_async_send_packet.html", null ],
+      [ "AsyncSpecialPacket", "interface_async_special_packet.html", null ],
+      [ "AsyncTCPSocket", "interface_async_t_c_p_socket.html", null ],
+      [ "AsyncUdpSocket", "interface_async_udp_socket.html", null ],
+      [ "<AsyncUdpSocketDelegate>", "protocol_async_udp_socket_delegate-p.html", [
+        [ "UDPCommander", "interface_u_d_p_commander.html", null ]
+      ] ],
+      [ "AsyncWritePacket", "interface_async_write_packet.html", null ],
+      [ "BWOrderedManagedObject", "interface_b_w_ordered_managed_object.html", [
+        [ "Action", "interface_action.html", null ],
+        [ "LocalPicture", "interface_local_picture.html", null ],
+        [ "Param", "interface_param.html", null ],
+        [ "Presentation", "interface_presentation.html", null ],
+        [ "Sequence", "interface_sequence.html", null ]
+      ] ],
+      [ "BWOrderedValueProxy", "interface_b_w_ordered_value_proxy.html", null ],
+      [ "Commander", "interface_commander.html", [
+        [ "TCPCommander", "interface_t_c_p_commander.html", null ],
+        [ "UDPCommander", "interface_u_d_p_commander.html", null ]
+      ] ],
+      [ "Config", "interface_config.html", null ],
+      [ "Database", "interface_database.html", null ],
+      [ "DataCache", "interface_data_cache.html", null ],
+      [ "FavoritesViewController", "interface_favorites_view_controller.html", null ],
+      [ "FlowCoverRecord", "interface_flow_cover_record.html", null ],
+      [ "FlowCoverView", "interface_flow_cover_view.html", null ],
+      [ "<FlowCoverViewDelegate>", "protocol_flow_cover_view_delegate-p.html", [
+        [ "SequenceChoiceViewController", "interface_sequence_choice_view_controller.html", null ]
+      ] ],
+      [ "HudDemoAppDelegate", "interface_hud_demo_app_delegate.html", null ],
+      [ "ImageDownloader", "interface_image_downloader.html", null ],
+      [ "Importer", "interface_importer.html", null ],
+      [ "MBProgressHUD", "interface_m_b_progress_h_u_d.html", null ],
+      [ "<MBProgressHUDDelegate>", "protocol_m_b_progress_h_u_d_delegate-p.html", [
+        [ "HudDemoViewController", "interface_hud_demo_view_controller.html", null ],
+        [ "RootViewController", "interface_root_view_controller.html", null ]
+      ] ],
+      [ "MBRoundProgressView", "interface_m_b_round_progress_view.html", null ],
+      [ "NSObject(AsyncSocketDelegate)", "interface_n_s_object_07_async_socket_delegate_08.html", null ],
+      [ "SettingsViewController", "interface_settings_view_controller.html", null ],
+      [ "statement", "classstatement.html", null ],
+      [ "SyncEvent", "interface_sync_event.html", null ],
+      [ "SynchronizerConnection", "interface_synchronizer_connection.html", null ],
+      [ "TCPServer", "interface_t_c_p_server.html", null ],
+      [ "<TCPServerDelegate>", "protocol_t_c_p_server_delegate-p.html", [
+        [ "Synchronizer", "interface_synchronizer.html", null ]
+      ] ],
+      [ "ZigPadAppDelegate", "interface_zig_pad_app_delegate.html", null ],
+      [ "ZigPadSettings", "interface_zig_pad_settings.html", null ]
+    ] ],
+    [ "Data Fields", "functions.html", null ],
+    [ "File List", "files.html", [
+      [ "ZigPad/ActionViewController.h", "_action_view_controller_8h.html", null ],
+      [ "ZigPad/ActionViewController.m", "_action_view_controller_8m.html", null ],
+      [ "ZigPad/AnimatorHelper.h", "_animator_helper_8h.html", null ],
+      [ "ZigPad/AnimatorHelper.m", "_animator_helper_8m.html", null ],
+      [ "ZigPad/AsyncTCPSocket.h", "_async_t_c_p_socket_8h.html", null ],
+      [ "ZigPad/AsyncTCPSocket.m", "_async_t_c_p_socket_8m.html", null ],
+      [ "ZigPad/AsyncUdpSocket.h", "_async_udp_socket_8h.html", null ],
+      [ "ZigPad/AsyncUdpSocket.m", "_async_udp_socket_8m.html", null ],
+      [ "ZigPad/Commander.h", "_commander_8h.html", null ],
+      [ "ZigPad/Commander.m", "_commander_8m.html", null ],
+      [ "ZigPad/CommandViewController.h", "_command_view_controller_8h.html", null ],
+      [ "ZigPad/CommandViewController.m", "_command_view_controller_8m.html", null ],
+      [ "ZigPad/Config.h", "_config_8h.html", null ],
+      [ "ZigPad/Config.m", "_config_8m.html", null ],
+      [ "ZigPad/DataCache.h", "_data_cache_8h.html", null ],
+      [ "ZigPad/DataCache.m", "_data_cache_8m.html", null ],
+      [ "ZigPad/FavoritesViewController.h", "_favorites_view_controller_8h.html", null ],
+      [ "ZigPad/FavoritesViewController.m", "_favorites_view_controller_8m.html", null ],
+      [ "ZigPad/FlowCoverView.h", "_flow_cover_view_8h.html", null ],
+      [ "ZigPad/FlowCoverView.m", "_flow_cover_view_8m.html", null ],
+      [ "ZigPad/ImageDownloader.h", "_image_downloader_8h.html", null ],
+      [ "ZigPad/ImageDownloader.m", "_image_downloader_8m.html", null ],
+      [ "ZigPad/ImageViewController.h", "_image_view_controller_8h.html", null ],
+      [ "ZigPad/ImageViewController.m", "_image_view_controller_8m.html", null ],
+      [ "ZigPad/Importer.h", "_importer_8h.html", null ],
+      [ "ZigPad/Importer.m", "_importer_8m.html", null ],
+      [ "ZigPad/main.m", "main_8m.html", null ],
+      [ "ZigPad/RootViewController.h", "_root_view_controller_8h.html", null ],
+      [ "ZigPad/RootViewController.m", "_root_view_controller_8m.html", null ],
+      [ "ZigPad/SequenceChoiceViewController.h", "_sequence_choice_view_controller_8h.html", null ],
+      [ "ZigPad/SequenceChoiceViewController.m", "_sequence_choice_view_controller_8m.html", null ],
+      [ "ZigPad/SettingsViewController.h", "_settings_view_controller_8h.html", null ],
+      [ "ZigPad/SettingsViewController.m", "_settings_view_controller_8m.html", null ],
+      [ "ZigPad/SyncEvent.h", "_sync_event_8h.html", null ],
+      [ "ZigPad/SyncEvent.m", "_sync_event_8m.html", null ],
+      [ "ZigPad/Synchronizer.h", "_synchronizer_8h.html", null ],
+      [ "ZigPad/Synchronizer.m", "_synchronizer_8m.html", null ],
+      [ "ZigPad/SynchronizerConnection.h", "_synchronizer_connection_8h.html", null ],
+      [ "ZigPad/SynchronizerConnection.m", "_synchronizer_connection_8m.html", null ],
+      [ "ZigPad/TCPCommander.h", "_t_c_p_commander_8h.html", null ],
+      [ "ZigPad/TCPCommander.m", "_t_c_p_commander_8m.html", null ],
+      [ "ZigPad/TCPServer.h", "_t_c_p_server_8h.html", null ],
+      [ "ZigPad/TCPServer.m", "_t_c_p_server_8m.html", null ],
+      [ "ZigPad/UDPCommander.h", "_u_d_p_commander_8h.html", null ],
+      [ "ZigPad/UDPCommander.m", "_u_d_p_commander_8m.html", null ],
+      [ "ZigPad/VideoViewController.h", "_video_view_controller_8h.html", null ],
+      [ "ZigPad/VideoViewController.m", "_video_view_controller_8m.html", null ],
+      [ "ZigPad/WebCamViewController.h", "_web_cam_view_controller_8h.html", null ],
+      [ "ZigPad/WebCamViewController.m", "_web_cam_view_controller_8m.html", null ],
+      [ "ZigPad/ZigPadAppDelegate.h", "_zig_pad_app_delegate_8h.html", null ],
+      [ "ZigPad/ZigPadAppDelegate.m", "_zig_pad_app_delegate_8m.html", null ],
+      [ "ZigPad/ZigPadSettings.h", "_zig_pad_settings_8h.html", null ],
+      [ "ZigPad/ZigPadSettings.m", "_zig_pad_settings_8m.html", null ],
+      [ "ZigPad/coredata/Action.h", "_action_8h.html", null ],
+      [ "ZigPad/coredata/Action.m", "_action_8m.html", null ],
+      [ "ZigPad/coredata/BWOrderedManagedObject.h", "_b_w_ordered_managed_object_8h.html", null ],
+      [ "ZigPad/coredata/BWOrderedManagedObject.m", "_b_w_ordered_managed_object_8m.html", null ],
+      [ "ZigPad/coredata/Database.h", "_database_8h.html", null ],
+      [ "ZigPad/coredata/Database.m", "_database_8m.html", null ],
+      [ "ZigPad/coredata/LocalPicture.h", "_local_picture_8h.html", null ],
+      [ "ZigPad/coredata/LocalPicture.m", "_local_picture_8m.html", null ],
+      [ "ZigPad/coredata/Param.h", "_param_8h.html", null ],
+      [ "ZigPad/coredata/Param.m", "_param_8m.html", null ],
+      [ "ZigPad/coredata/Presentation.h", "_presentation_8h.html", null ],
+      [ "ZigPad/coredata/Presentation.m", "_presentation_8m.html", null ],
+      [ "ZigPad/coredata/Sequence.h", "_sequence_8h.html", null ],
+      [ "ZigPad/coredata/Sequence.m", "_sequence_8m.html", null ],
+      [ "ZigPad/MBProgressHUD/MBProgressHUD.h", "_m_b_progress_h_u_d_8h.html", null ],
+      [ "ZigPad/MBProgressHUD/MBProgressHUD.m", "_m_b_progress_h_u_d_8m.html", null ],
+      [ "ZigPad/MBProgressHUD/Demo/main.m", "_m_b_progress_h_u_d_2_demo_2main_8m.html", null ],
+      [ "ZigPad/MBProgressHUD/Demo/Classes/HudDemoAppDelegate.h", "_hud_demo_app_delegate_8h.html", null ],
+      [ "ZigPad/MBProgressHUD/Demo/Classes/HudDemoAppDelegate.m", "_hud_demo_app_delegate_8m.html", null ],
+      [ "ZigPad/MBProgressHUD/Demo/Classes/HudDemoViewController.h", "_hud_demo_view_controller_8h.html", null ],
+      [ "ZigPad/MBProgressHUD/Demo/Classes/HudDemoViewController.m", "_hud_demo_view_controller_8m.html", null ]
+    ] ],
+    [ "Globals", "globals.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
